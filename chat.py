c9459ebb22c9fd0295299dfbcff96e2bc4a33ee7
Sean Currlin <sean.currlin@gmail.com>
2024-07-28 09:07:08 -0400

    add goodbye message

--- chat.py
+++ chat.py
@@ -21,6 +21,7 @@ if __name__ == "__main__":
     while True:
         user_input = input("You: ")
         if user_input.lower() in ["quit", "exit", "bye"]:
+            print("Bye! Have a great day!")
             break
 
         response = chat_with_gpt(user_input)
